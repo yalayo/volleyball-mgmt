2c463fc9c4844038cf57ef6ce3df49bd6ac59d9a
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-07 15:11:54 +0200

    Wrote initial implementation for the base.

--- bases/results-scraper/src/app/backend/results_scraper/core.clj
+++ bases/results-scraper/src/app/backend/results_scraper/core.clj
@@ -1,1 +1,14 @@
-(ns app.backend.results-scraper.core)
+(ns app.backend.results-scraper.core
+  (:require [com.brunobonacci.mulog :as μ])
+  (:gen-class))
+
+(μ/set-global-context!
+ {:app-name "results-scraper", :version "0.0.1", :env "prod"})
+
+(μ/start-publisher! {:type :console})
+
+;; Check how to use credentials (μ/start-publisher! {:type :cloudwatch :group-name "volleyball-3.0"})
+
+(defn -main [& args]
+  (println args)
+  (μ/log ::lambda-execution, :args args))
