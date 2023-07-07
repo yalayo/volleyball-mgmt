76579e41298ab9e4d62b067c759790527b1c0d70
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-07 15:08:33 +0200

    Created a base and included it in the projects.

---
+++ bases/results-scraper/src/app/backend/results_scraper/core.clj
@@ -0,0 +1,1 @@
+(ns app.backend.results-scraper.core)
---
+++ bases/results-scraper/test/app/backend/results_scraper/core_test.clj
@@ -0,0 +1,6 @@
+(ns app.backend.results-scraper.core-test
+  (:require [clojure.test :as test :refer :all]
+            [app.backend.results-scraper.core :as core]))
+
+(deftest dummy-test
+  (is (= 1 1)))
