25e1623f37fd6685f39f5e9564af0b1d70e49865
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-11 18:11:14 +0200

    Added initial terraform code for the lambda function.

---
+++ projects/scraper-lambda/terraform/main.tf
@@ -0,0 +1,30 @@
+# get the policy to be used
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# create role to use the lambda service
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# create the lambda where to deploy this project on
+resource "aws_lambda_function" "target_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "function.zip"
+  function_name = "scraper_lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
+
+  runtime = "provided"
+}
