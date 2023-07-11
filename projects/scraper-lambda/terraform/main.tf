7fd98e3b1160933ba77aeac4288e4c17b52a0335
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-11 21:05:01 +0200

    Small change in the path to find the code.

--- projects/scraper-lambda/terraform/main.tf
+++ projects/scraper-lambda/terraform/main.tf
@@ -45,7 +45,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 resource "aws_lambda_function" "target_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "function.zip"
+  filename      = "../../function.zip"
   function_name = "scraper_lambda"
   handler       = "executable"
   role          = aws_iam_role.iam_for_lambda.arn
