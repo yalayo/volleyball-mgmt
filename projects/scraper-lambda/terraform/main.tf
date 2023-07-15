5377e22fc19719cb22003d04634ae26d399887ef
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-15 12:13:53 +0200

    Added fuction name.

--- projects/scraper-lambda/terraform/main.tf
+++ projects/scraper-lambda/terraform/main.tf
@@ -43,6 +43,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 # create the lambda where to deploy this project on
 resource "aws_lambda_function" "target_lambda" {
+  function_name = "scraper-lambda"
   package_type  = "Image"
   image_uri     = "rondonsarnik/scraper-lambda"
   role          = aws_iam_role.iam_for_lambda.arn
