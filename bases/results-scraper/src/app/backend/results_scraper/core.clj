b9a4b5477949a86b5c14d2372f29232d0f0c0305
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-18 07:45:50 +0200

    Initial test of running scheduled tasks.

--- bases/results-scraper/src/app/backend/results_scraper/core.clj
+++ bases/results-scraper/src/app/backend/results_scraper/core.clj
@@ -1,5 +1,7 @@
 (ns app.backend.results-scraper.core
-  (:require [com.brunobonacci.mulog :as μ])
+  (:require [com.brunobonacci.mulog :as μ]
+            [chime.core :as chime])
+  (:import [java.time Instant Duration])
   (:gen-class))
 
 (μ/set-global-context!
@@ -10,5 +12,8 @@
 ;; Check how to use credentials (μ/start-publisher! {:type :cloudwatch :group-name "volleyball-3.0"})
 
 (defn -main [& args]
-  (println args)
-  (μ/log ::lambda-execution, :args args))
+  (chime/chime-at (-> (chime/periodic-seq (Instant/now) (Duration/ofSeconds 3))
+                      rest)
+                  (fn [time]
+                    (μ/log ::task-execution :args args :message (str "Chiming at" time))
+                    (println "Chiming at" time))))
