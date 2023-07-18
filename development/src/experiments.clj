446c480133e9ec22bbb27393933e851b504706a7
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-18 17:22:06 +0200

    Working with scheduled tasks.

--- development/src/experiments.clj
+++ development/src/experiments.clj
@@ -1,1 +1,10 @@
 (ns experiments)
+
+(require '[chime.core :as chime])
+(import '[java.time Instant Duration])
+
+(let [now (Instant/now)]
+  (chime/chime-at (-> (chime/periodic-seq (Instant/now) (Duration/ofSeconds 3))
+                      rest)
+                  (fn [time]
+                    (println "Chiming at" time))))
