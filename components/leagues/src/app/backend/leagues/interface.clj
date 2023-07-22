a4e1d83b56055f41aa0aa63d1e47b26cfe8aa38c
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-22 21:29:42 +0200

    Created a new component to handle the leagues.

---
+++ components/leagues/src/app/backend/leagues/interface.clj
@@ -0,0 +1,1 @@
+(ns app.backend.leagues.interface)
---
+++ components/leagues/test/app/backend/leagues/interface_test.clj
@@ -0,0 +1,6 @@
+(ns app.backend.leagues.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [app.backend.leagues.interface :as leagues]))
+
+(deftest dummy-test
+  (is (= 1 1)))
