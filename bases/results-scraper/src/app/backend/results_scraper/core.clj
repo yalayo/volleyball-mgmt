27556b0356f410d01a7a9296929ada62c9e6863b
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-24 07:08:22 +0200

    Store leagues data for the first time when the system starts.

--- bases/results-scraper/src/app/backend/results_scraper/core.clj
+++ bases/results-scraper/src/app/backend/results_scraper/core.clj
@@ -1,16 +1,23 @@
 (ns app.backend.results-scraper.core
   (:require [com.brunobonacci.mulog :as μ] 
             [app.backend.database.interface :as database]
-            [app.backend.results-scraper.scheduler :as scheduler]) 
+            [app.backend.results-scraper.scheduler :as scheduler]
+            [app.backend.leagues.interface :as leagues]) 
   (:gen-class))
 
 (μ/set-global-context!
  {:app-name "results-scraper", :version "0.0.1", :env "prod"})
 
-(μ/start-publisher! {:type :console})
+(μ/start-publisher!
+  {:type :multi
+   :publishers
+   [{:type :console}
+    #_{:type :cloudwatch :group-name "volleyball"}]})
 
 ;; Check how to use credentials (μ/start-publisher! {:type :cloudwatch :group-name "volleyball-3.0"})
 
 (defn -main [& args]
   (database/init)
+  (leagues/store-schema)
+  (leagues/store-leagues-data)
   (scheduler/schedule))
