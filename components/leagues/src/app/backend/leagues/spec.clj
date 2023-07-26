2e2747e0ee54d51bdd2219c3a855c8e031cd067b
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-26 09:30:13 +0200

    Updated schema and specs.

--- components/leagues/src/app/backend/leagues/spec.clj
+++ components/leagues/src/app/backend/leagues/spec.clj
@@ -1,10 +1,11 @@
 (ns app.backend.leagues.spec
   (:require [clojure.spec.alpha :as s]))
 
+(s/def ::league-id string?)
 (s/def ::name string?)
 (s/def ::category (s/and keyword? #{:ober-klasse :lande :bezirk :jungen}))
 (s/def ::sub-category string?)
 (s/def ::url string?)
 (s/def ::area string?)
 
-(s/def ::league-data (s/keys :req-un [::name  ::category ::sub-category ::url ::area]))
+(s/def ::league-data (s/keys :req-un [::league-id ::name  ::category ::sub-category ::url ::area]))
--- components/leagues/src/app/backend/leagues/storage.clj
+++ components/leagues/src/app/backend/leagues/storage.clj
@@ -2,20 +2,22 @@
   (:require [app.backend.database.interface :as database]))
 
 (def ^:private db-schema
-  [{:db/ident :league/name
+  [{:db/ident :league-id
     :db/valueType :db.type/string
-    :db/unique :db.unique/identity
     :db/cardinality :db.cardinality/one}
-   {:db/ident :league/category
+   {:db/ident :name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :category
     :db/valueType :db.type/symbol
     :db/cardinality :db.cardinality/one}
-   {:db/ident :league/sub-category
+   {:db/ident :sub-category
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}
-   {:db/ident :league/area
+   {:db/ident :area
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}
-   {:db/ident :league/url
+   {:db/ident :url
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}])
 
