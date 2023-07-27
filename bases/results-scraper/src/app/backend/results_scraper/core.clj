3094be80ddcf8000c71a46e8c5d42033bcd73fe8
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-27 17:15:58 +0200

    Updated code to use another log publisher.

--- bases/results-scraper/src/app/backend/results_scraper/core.clj
+++ bases/results-scraper/src/app/backend/results_scraper/core.clj
@@ -1,5 +1,6 @@
 (ns app.backend.results-scraper.core
-  (:require [com.brunobonacci.mulog :as μ] 
+  (:require [com.brunobonacci.mulog :as μ]
+            [cognitect.aws.credentials :as credentials]
             [app.backend.database.interface :as database]
             [app.backend.results-scraper.scheduler :as scheduler]
             [app.backend.results-scraper.repl :as repl]
@@ -9,15 +10,28 @@
 (μ/set-global-context!
  {:app-name "results-scraper", :version "0.0.1", :env "prod"})
 
-(μ/start-publisher!
-  {:type :multi
-   :publishers
-   [{:type :console}
-    #_{:type :cloudwatch :group-name "volleyball"}]})
+(defn- init-logging []
+  (let [publisher {:type :multi}
+        publishers [{:type :console}
+                    {:type        :slack
+                     :webhook-url "https://hooks.slack.com/services/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"}]
+        cloudwatch {:type :cloudwatch 
+                    :group-name "volleyball" 
+                    :cloudwatch-client-config {:api :logs
+                                               :credentials-provider (credentials/basic-credentials-provider
+                                                                      {:access-key-id (System/getenv "AWS_ACCESS_KEY_ID")
+                                                                       :secret-access-key (System/getenv "AWS_SECRET_ACCESS_KEY")})}}]
+    (when (not (nil? (System/getenv "PLACE_HOLDER")))
+      (assoc publisher :publishers (conj publishers cloudwatch)))
+    (assoc publisher :publishers publishers)))
 
-;; Check how to use credentials (μ/start-publisher! {:type :cloudwatch :group-name "volleyball-3.0"})
+(defn- init []
+  (println (init-logging))
+  (μ/start-publisher! (init-logging)))
+
 
 (defn -main [& args]
+  (init)
   (repl/init)
   (database/init)
   #_(leagues/store-schema)
