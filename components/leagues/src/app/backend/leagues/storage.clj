bfe4e2617f995b5d220f248333a60b9461960c30
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-28 09:48:56 +0200

    Added method to detect only the new schema to transact.

--- components/leagues/src/app/backend/leagues/storage.clj
+++ components/leagues/src/app/backend/leagues/storage.clj
@@ -22,10 +22,59 @@
     :db/cardinality :db.cardinality/one}
    {:db/ident :url
     :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :team-id
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-id
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-season
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-league
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-item-id
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-item-parent
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-item-team
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-item-place
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-item-games
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-item-scores
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-item-sets
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :standing-item-points
+    :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}])
 
 (defn store-schema []
-  (database/transact db-schema))
+  (let [result (database/query "[:find ?a :where [_ :db/ident ?a]]")
+        new-schema (atom [])]
+    (doseq [item db-schema]
+      (when-not (contains? result [(:db/ident item)])
+        (swap! new-schema #(conj % item))))
+    (database/transact @new-schema)))
 
 (defn store-leagues-data [data]
   (database/transact data))
+
+(defn leagues-to-scan []
+  (let [data (database/query "[:find ?n ?a :where [?e :league-id ?n][?e :url ?a]]")
+        keys [:league-id :url]
+        result (atom [])]
+    (doseq [item data]
+      (swap! result #(conj % (zipmap keys item))))
+    @result))
