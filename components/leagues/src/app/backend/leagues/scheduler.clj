d189ac89ea691e6365d01d5e8d686afead9e3705
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-07-28 15:06:32 +0200

    Moved the scheduling code to the leagues component.

---
+++ components/leagues/src/app/backend/leagues/scheduler.clj
@@ -0,0 +1,17 @@
+(ns app.backend.leagues.scheduler
+  (:require [com.brunobonacci.mulog :as μ]
+            [chime.core :as chime]
+            [clojure.core.async :refer [<!!]]
+            [app.backend.leagues.scraper :as scraper])
+  (:import [java.time LocalTime ZonedDateTime ZoneId Period Instant Duration]
+           [java.io FileNotFoundException]))
+
+(defn schedule-standings-scans [channel]
+  (chime/chime-at (-> (chime/periodic-seq (Instant/now) (Duration/ofMinutes 1))
+                      rest)
+                  (fn [time]
+                    (let [data (<!! channel)]
+                      (if-some [data nil]
+                        (μ/trace :standing-scanned :message (str "Scanned at " time) []
+                                 (scraper/scan-table data))
+                        (μ/log :standing-scanned :message "Process finished for " data))))))
